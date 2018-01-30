77b918ae11b0195615ef28303fbab4876baf8a0a
Nathan Dunn <ndunn@me.com>
2018-01-29 16:52:44 -0800

    added a gene list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import tgacPathways from './data/tgac'
 
 var xenaQuery = require('ucsc-xena-client/dist/xenaQuery');
 var Rx = require('ucsc-xena-client/dist/rx');
+
 // var Rx = require('rx');
 
 
@@ -36,7 +37,7 @@ console.log(tgacPathways);
 // otherwies use fetch-Cohort-preferred
 
 var geneList = [
-'RB1','CALN1'
+    'ABCA13', 'ABCB5', 'ACTB', 'ADCYAP1R1', 'AEBP1', 'AHR', 'AKR1B1', 'AP4M1', 'AQP1', 'ARHGEF5', 'ASL', 'BBS9', 'BCL7B', 'BZW2', 'C7orf31', 'C7orf36', 'C7orf43', 'C7orf51', 'CBLL1', 'CCDC129', 'CD36', 'CDC14C', 'CDC2L5', 'CHCHD2', 'CHN2', 'CLIP2', 'COL1A2', 'COL28A1', 'CYP2W1', 'CYTH3', 'DBF4', 'DGKB', 'DKFZp564N2472', 'DNAH11', 'ECOP', 'EIF3B', 'EIF3S9', 'ENSG00000179994', 'ENSG00000197320', 'EPDR1', 'ETV1', 'FAM126A', 'FBXO24', 'GCK', 'GHRHR', 'GLI3', 'GPER', 'GPNMB', 'GPR85', 'GTF2IRD2B', 'GUSB', 'HECW1', 'HOXA1', 'HOXA13', 'HOXA2', 'IGFBP1', 'INHBA', 'IRF5', 'LAMB1', 'LANCL2', 'LOC100131675', 'LOC100131859', 'LOC100133120', 'LOC100134747', 'LOC346329', 'LOC389465', 'LOC402509', 'LOC441228', 'LOC442572', 'LOC442668', 'LOC641765', 'LOC641922', 'LRWD1', 'MACC1', 'MAD1L1', 'MGC72080', 'MTERF', 'MUC17', 'NFE2L3', 'NPSR1', 'OSBPL3', 'PAPOLB', 'PDE1C', 'PHF14', 'PMS2', 'PMS2CL', 'PMS2L5', 'PON3', 'PRKAR1B', 'RAPGEF5', 'RELN', 'RINT1', 'RSPH10B', 'SCIN', 'SDK1', 'SEMA3A', 'SEMA3C', 'SEMA3D', 'SEPT14', 'SKAP2', 'SLC13A1', 'SLC29A4', 'STAG3', 'STEAP1', 'STK31', 'TBRG4', 'TMEM130', 'TMEM184A', 'TMEM196', 'TNPO3', 'VSTM2A', 'ZMIZ2', 'ZNF12', 'ZNF138', 'uc003vzh.2'
 ];
 
 var dataSetIdDemo = 'TCGA.OV.sampleMap/mutation_wustl';
@@ -49,13 +50,13 @@ var {allCohorts, cohortSamples, fetchCohortPreferred, sparseData} = xenaQuery;
 
 allCohorts('https://tcga.xenahubs.net')
     .flatMap((cohorts) => {
-        console.log('first cohort: '+ cohorts[0] + ' vs '+ dataSetCohortDemo);
+        console.log('first cohort: ' + cohorts[0] + ' vs ' + dataSetCohortDemo);
         return cohortSamples('https://tcga.xenahubs.net', dataSetCohortDemo, null)
     })
     .flatMap((sampleList) => {
         return Rx.Observable.zipArray(
             geneList.map(gene => {
-                return sparseData('https://tcga.xenahubs.net', dataSetIdDemo, sampleList,gene)
+                return sparseData('https://tcga.xenahubs.net', dataSetIdDemo, sampleList, gene)
             })
         );
     })
